b5f0ec5f22200673c73daa81da4b9ca0a26fe75e
弥生 重松 <mirai3.2.1993@gmail.com>
2018-08-30 15:05:19 +0900

    http リクエストを送った際のレスポンス用の構造体を作成

---
+++ src/HttpClientResponseEntity.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miraishigematsu
+ * Date: 2018/08/30
+ * Time: 14:13
+ */
+
+namespace Src;
+
+
+class HttpClientResponseEntity
+{
+  public $statusCode = 0;
+  public $headers = [];
+  public $body = '';
+}
